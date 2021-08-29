43209d2703b52b6495ce643c6e6359205b04349e
chunza2542 <hello@thechun.dev>
2021-08-29 17:07:16 +0700

    Remove serverless-infrastructure

--- packages/serverless-infrastructure/lib/index.ts
+++
@@ -1,7 +0,0 @@
-import * as sst from "@serverless-stack/resources";
-
-import DynamoDBStack from "./DynamoDBStack";
-
-export default function main(app: sst.App): void {
-  new DynamoDBStack(app, "dynamodb");
-}
