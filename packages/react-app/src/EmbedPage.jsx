7d88a0ef5942b530c2f798d77107542995ddeb02
chunza2542 <hello@thechun.dev>
2021-08-29 10:38:33 +0700

    Design comment layout

--- packages/react-app/src/EmbedPage.jsx
+++ packages/react-app/src/EmbedPage.jsx
@@ -1,5 +1,38 @@
+import styled from "styled-components";
+import Comment from "./components/Comment";
+import { Divider } from "antd";
+
+const Footer = styled.span`
+  display: block;
+  color: #555;
+  font-weight: normal;
+  font-size: 0.8rem;
+`;
+
 const EmbedPage = () => {
-  return <div>Embed Page</div>;
+  return (
+    <>
+      <h3>26 comments</h3>
+      <Comment>
+        <Comment />
+        <Comment />
+        <Comment />
+      </Comment>
+      <Comment>
+        <Comment />
+        <Comment />
+        <Comment />
+      </Comment>
+      <Divider />
+      <div>Comment Writing</div>
+      <Footer>
+        Powered by{" "}
+        <a href="https://ethtalk.app" target="_blank">
+          EthTalk
+        </a>
+      </Footer>
+    </>
+  );
 };
 
 export default EmbedPage;
