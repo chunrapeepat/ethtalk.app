3eb1d346311a3ad47980c295f8d2f173fe4cf34a
chunza2542 <hello@thechun.dev>
2021-09-04 15:58:09 +0700

    Latex support (mathjax)

--- packages/react-app/src/components/Comment.jsx
+++ packages/react-app/src/components/Comment.jsx
@@ -12,6 +12,7 @@ import { hashURL } from "../utils/helper";
 import CommentEditor from "./CommentEditor";
 import { Button, OutlineButton } from "./Button";
 import Reply from "./Reply";
+import MathJax from "react-mathjax2";
 
 const LikeButton = styled.div`
   display: flex;
@@ -251,7 +252,29 @@ const Comment = ({ id, authorPublicAddress, createdAt, data, commentURL }) => {
         content={
           // TODO: support latex and markdown
           <>
-            {!isEdit && <p>{data}</p>}
+            {!isEdit && (
+              <p>
+                <MathJax.Context
+                  input="ascii"
+                  onLoad={() => console.log("Loaded MathJax script!")}
+                  onError={(MathJax, error) => {
+                    console.warn(error);
+                    console.log("Encountered a MathJax error, re-attempting a typeset!");
+                    MathJax.Hub.Queue(MathJax.Hub.Typeset());
+                  }}
+                  script="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=AM_HTMLorMML"
+                  options={{
+                    asciimath2jax: {
+                      useMathMLspacing: true,
+                      delimiters: [["$$", "$$"]],
+                      preview: "none",
+                    },
+                  }}
+                >
+                  <MathJax.Text text={data} />
+                </MathJax.Context>
+              </p>
+            )}
             {isEdit && <CommentEditor value={editedValue} onChange={setEditedValue} footer={editEditorFooter} />}
           </>
         }
--- packages/react-app/src/components/Reply.jsx
+++ packages/react-app/src/components/Reply.jsx
@@ -11,6 +11,7 @@ import { firestore } from "../utils/firebase";
 import { hashURL } from "../utils/helper";
 import { OutlineButton, Button } from "./Button";
 import CommentEditor from "./CommentEditor";
+import MathJax from "react-mathjax2";
 
 const PanelContainer = styled.div`
   display: flex;
@@ -157,7 +158,29 @@ const Reply = ({ commentId, id, authorPublicAddress, createdAt, data, commentURL
       content={
         // TODO: support latex and markdown
         <>
-          {!isEdit && <p>{data}</p>}
+          {!isEdit && (
+            <p>
+              <MathJax.Context
+                input="ascii"
+                onLoad={() => console.log("Loaded MathJax script!")}
+                onError={(MathJax, error) => {
+                  console.warn(error);
+                  console.log("Encountered a MathJax error, re-attempting a typeset!");
+                  MathJax.Hub.Queue(MathJax.Hub.Typeset());
+                }}
+                script="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=AM_HTMLorMML"
+                options={{
+                  asciimath2jax: {
+                    useMathMLspacing: true,
+                    delimiters: [["$$", "$$"]],
+                    preview: "none",
+                  },
+                }}
+              >
+                <MathJax.Text text={data} />
+              </MathJax.Context>
+            </p>
+          )}
           {isEdit && <CommentEditor value={editedValue} onChange={setEditedValue} footer={editEditorFooter} />}
         </>
       }
